85e79e2bfff92d44c7796475bad9c34adbbf6e50
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2020-03-13 13:55:23 +0100

    First draft of randomize-group
    R code from comap package.

---
+++ src/sged-randomize-groups.py
@@ -0,0 +1,166 @@
+#! /usr/bin/python
+
+""" Created on 12/03/20 by jdutheil
+
+    Generates a list of groups with characteristics similar to a given set of groups.
+    The output groups have the same size and similar site porperties, but sites are taken randomly.
+    In this version, site norms are not discretized but a similarity threshold is used.
+    To remove bias due to skewed distribution of norms, a correction is further introduced.
+"""
+
+
+import getopt, sys, os.path
+import pandas
+import numpy
+
+cmd_args = sys.argv
+arg_list = cmd_args[1:]
+
+unix_opt = "s:p:o:m:g:a:c"
+full_opt = ["sged=", "pdb=", "output=", "measures=", "groups=", "chain=", "csv"]
+try:
+  arguments, values = getopt.getopt(arg_list, unix_opt, full_opt)
+except getopt.error as err:
+  print (str(err))
+  sys.exit(2)
+
+tabsep = True # TSV by default
+group_col = "Group"
+measures = []
+for arg, val in arguments:
+  if arg in ("-s", "--sged"):
+    sged_file = val
+    print "SGED file: %s" % sged_file
+  elif arg in ("-p", "--pdb"):
+    pdb_file = val
+    print "PDB file: %s" % pdb_file
+  elif arg in ("-o", "--output"):
+    output_file = val
+    print "Output info file: %s" % output_file
+  elif arg in ("-m", "--measures"):
+    measures = val.split(',')
+  elif arg in ("-g", "--groups"):
+    group_col = val
+    print "PDB coordinates are in column: %s" % group_col
+  elif arg in ("-a", "--chain"):
+    chain_sel = val
+    print "PDB chain to use: %s" % chain_sel
+  elif arg in ("-c", "--csv"):
+    tabsep = False
+
+if tabsep:
+  print "SGED file is in TSV format"
+  delim = '\t'
+else:
+  print "SGED file is in CSV format"
+  delim = ','
+
+
+
+# Input:
+sitesPath <- "Myo_sites.csv"
+groupsPath <- "Myo_stats_pvalues.csv"
+
+# Variable to codition over
+cond.var <- "N" #Norm
+
+# Number of randomizations to perform for each group:
+nrep <- 100
+
+# Similarity threshold used for norm:
+sim.t <- 0.1 #10% difference: (Nsim - Nobs) / Nobs < 0.1
+min.obs <- 5 #Minimum number of matching site. If not enough sites are found for a given position, a warning will be issued. You may try to increase the threshold if too many warnings are produced. 
+
+# Output path:
+outputPath <- "Myo_random.csv"
+
+###########################################
+
+# Read input data:
+sites <- read.table(sitesPath, header = TRUE, stringsAsFactors = FALSE)
+sites$Group <- substr(sites$Group, 2, nchar(sites$Group) - 1)
+row.names(sites) <- sites$Group
+groups <- read.table(groupsPath, header = TRUE, stringsAsFactors = FALSE)
+groupsLst <- strsplit(substr(groups$Group, 2, nchar(groups$Group) - 1), ";")
+
+# Now replicate each group:
+x.rep <- numeric(nrow(groups) * nrep)
+l.grp <- list()
+x.grp <- character(nrow(groups) * nrep)
+x.ave <- numeric(nrow(groups) * nrep) #Average of the sampled group
+x.siz <- numeric(nrow(groups) * nrep)
+x.oav <- numeric(nrow(groups) * nrep) #Average the original group
+i <- 1
+for (grp in 1:nrow(groups)) {
+  size <- groups[grp, "Size"]
+  nmin <- groups[grp, "Nmin"]
+  
+  # Get all sites with adequate value for each position:
+  gp <- groupsLst[[grp]]
+  if (length(gp) != size) stop("!!! Error in input file, group size does not match number or sites!")
+  gp.vals <- numeric(size)
+  for (j in 1:size) {
+    gp.vals[j] <- sites[gp[j], cond.var]
+  }
+
+  x.rep[i:(i + nrep - 1)] <- 1:nrep
+  x.siz[i:(i + nrep - 1)] <- size
+  x.grp[i:(i + nrep - 1)] <- "["
+  x.ave[i:(i + nrep - 1)] <- 0
+  x.oav[i:(i + nrep - 1)] <- mean(gp.vals)
+  
+  # Loop over each site in the group:
+  for (sit in 1:size) {
+    # Get all sites with similar rate (or any other conditional variable):
+    x <- sites[,cond.var]
+    t <- abs(x - gp.vals[sit]) / gp.vals[sit]
+    condSites <- subset(sites, t <= sim.t)
+    # Loop over each simulation replicate:
+    for (sim in 1:nrep) {    
+      # Remove sites already present in the group
+      if (i + sim - 1 <= length(l.grp)) { # test if element already exists in list
+        tmp <- subset(condSites, ! (Group %in% l.grp[[i + sim - 1]]))
+      } else {
+        tmp <- condSites
+      }
+
+      # Sampling correction:
+      tmpl <- tmp[tmp[,cond.var] < gp.vals[sit],]
+      tmpg <- tmp[tmp[,cond.var] > gp.vals[sit],]
+      tmpe <- tmp[tmp[,cond.var] == gp.vals[sit],]
+      n <- min(nrow(tmpl), nrow(tmpg))
+      n <- max(n, min.obs) # we add min.obs there to avoid getting no replicate site when we have extreme values for the candidate site.
+      tmp2 <- rbind(tmpl[sample(1:nrow(tmpl), min(n, nrow(tmpl))),], tmpe, tmpg[sample(1:nrow(tmpg), min(n, nrow(tmpg))),])
+      if (nrow(tmp2) == 0) {
+        warning(paste("No more similar site available for candidate site", sit, "in group", grp, "replicate", sim))
+        x.grp[i + sim - 1] <- paste(x.grp[i + sim - 1], "NA", sep = ifelse(x.grp[i + sim - 1] == "[", "", ";"))
+        x.ave[i + sim - 1] <- NA
+      } else {
+        if (nrow(tmp2) < min.obs)
+          warning(paste("Minimum site frequency not matched for candidate site", sit, "in group", grp, "replicate", sim))
+
+        # Now sample sites:
+        x <- sample(1:nrow(tmp2), 1)
+        if (i + sim - 1 <= length(l.grp)) {
+          l.grp[[i + sim - 1]] <- append(l.grp[[i + sim - 1]], tmp2[x, "Group"])
+        } else {
+          l.grp[[i + sim - 1]] <- tmp2[x, "Group"]
+        }
+        x.grp[i + sim - 1] <- paste(x.grp[i + sim - 1], tmp2[x, "Group"], sep = ifelse(x.grp[i + sim - 1] == "[", "", ";"))
+        x.ave[i + sim - 1] <- x.ave[i + sim - 1] + tmp2[x, cond.var]
+      }
+    }
+  }
+  x.grp[i:(i + nrep - 1)] <- paste(x.grp[i:(i + nrep - 1)], "]", sep = "")
+  i <- i + nrep
+}
+
+x.ave <- x.ave / x.siz
+results <- data.frame(Replicate = x.rep, Group = x.grp, Size = x.siz, RandMean = x.ave, OrigMean = x.oav)
+
+write.table(results, file = outputPath, sep = "\t", row.names = FALSE)
+
+
+
+print "Done."
+
