a6463ac4c0b6d1b20c1d82901e700a59606aa1c6
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2020-07-31 13:00:03 +0200

    Fixed issue with missing values

--- src/sged-translate-coords.py
+++ src/sged-translate-coords.py
@@ -57,7 +57,7 @@ index.dropna(inplace = True)
 
 # Start parsing
 with open(sged_file) as csv_file:
-  df = pandas.read_csv(csv_file, sep = delim, dtype = str)
+  df = pandas.read_csv(csv_file, sep = delim, dtype = str, keep_default_na = False) #NA in columns ignored
   groups = df["Group"]
   df.drop("Group", axis = 1, inplace = True)
   with open(output_file, "w") as handle:
