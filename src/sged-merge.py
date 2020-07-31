4356978cbd48cd574a38dce4d17c4dfbfaf8d1a0
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2020-07-31 15:35:46 +0200

    Merge allows to specify index columns in both files.

--- src/sged-merge.py
+++ src/sged-merge.py
@@ -11,8 +11,8 @@ import pandas
 cmd_args = sys.argv
 arg_list = cmd_args[1:]
 
-unix_opt = "s:t:o:g:j:c"
-full_opt = ["sged1=", "sged2=", "output=", "groups=", "join=", "csv"]
+unix_opt = "s:t:o:g:h:j:c"
+full_opt = ["sged1=", "sged2=", "output=", "groups=", "groups1=", "groups2=", "join=", "csv"]
 try:
   arguments, values = getopt.getopt(arg_list, unix_opt, full_opt)
 except getopt.error as err:
@@ -20,7 +20,8 @@ except getopt.error as err:
   sys.exit(2)
 
 tabsep = True # TSV by default
-group_col = "Group"
+group_col1 = "Group"
+group_col2 = "Group"
 measures = []
 join_type = "outer"
 for arg, val in arguments:
@@ -33,9 +34,12 @@ for arg, val in arguments:
   elif arg in ("-o", "--output"):
     output_file = val
     print "Output merged file: %s" % output_file
-  elif arg in ("-g", "--groups"):
-    group_col = val
-    print "PDB coordinates are in column: %s" % group_col
+  elif arg in ("-g", "--groups", "--groups1"): # Note: if only this arg is passed, group col name is assumed to be identical in both files
+    group_col1 = val
+    print "Coordinates are in column: %s" % group_col1
+  elif arg in ("-h", "--groups2"):
+    group_col2 = val
+    print "Coordinates for second file are in column: %s" % group_col2
   elif arg in ("-j", "--join"):
     join_type =  val
     print "Join type: %s" % join_type
@@ -56,7 +60,7 @@ with open(sged_file2) as csv_file2:
   df2 = pandas.read_csv(csv_file2, sep = delim, dtype = str, comment = '#')
 
 frames = [df1, df2]
-df = pandas.merge(df1, df2, how = join_type, on = group_col)
+df = pandas.merge(df1, df2, how = join_type, left_on = group_col1, right_on = group_col2)
 
 # Write results:
 df.to_csv(output_file, sep = delim, na_rep = 'NA', index = False)
